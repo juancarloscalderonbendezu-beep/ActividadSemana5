1fdd099c6b045e90a8257713ce081120fcc21b14
juancarloscalderonbendezu <juancarloscalderonbendezu@gmail.com>
2026-09-11 19:27:10 -0500

    commit para manejo de versiones

--- archivo.java
+++ archivo.java
@@ -1,5 +1,7 @@
 public class archivo {
     public static void main(String[] args) {
         System.out.println("Esto es un archivo de prueba ");
+        System.out.println("esta es una nueva linea para el manejo de versiones  ");
+
     }
 }
